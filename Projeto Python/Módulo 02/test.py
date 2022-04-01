fad1ed484fa54b6162819fdeb7c84d4607950adb
carlositaloo <carlos_italo@email.com>
2022-04-01 15:05:05 -0300

    ok

--- Projeto Python/Módulo 02/test.py
+++ Projeto Python/Módulo 02/test.py
@@ -1,7 +1,8 @@
-a = str(input('Digite uma frase: '))
-a1 = ((a.split())[0:])
-a2 = (''.join(a1))
-if a2 == (a2[::-1]):
-    print('{} é um palindromo!'.format(a))
-else:
-    print('( {} )não é a mesma coisa ao contrario ( {} ) então não é um palindromo.'.format(a2, (a2[::-1])))
+from os import system
+
+system('cls')
+
+n1 = int(input('Digite um número para saber seu fatorial: '))
+
+for c in range(0, n1, -1):
+    print('oie')

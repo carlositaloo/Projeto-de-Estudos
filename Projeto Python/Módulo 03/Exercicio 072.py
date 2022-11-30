412bfee3772e24b592f331c34aef69d0bb5e18bb
iNooTh <carlos_italo@email.com>
2022-11-29 23:09:45 -0300

    OK

--- Projeto Python/Módulo 03/Exercicio 072.py
+++ Projeto Python/Módulo 03/Exercicio 072.py
@@ -1,3 +1,8 @@
+"""
+Exercício Python 72: Crie um programa que tenha uma dupla totalmente preenchida com uma contagem por
+extenso, de zero até vinte. Seu programa deverá ler um número pelo teclado (entre 0 e 20) e
+mostrá-lo por extenso.
+"""
 from os import system
 
 system('cls')
